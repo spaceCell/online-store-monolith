d7f5942a11d94d297636492ba4f2b537c6dc4366
roadthelife <kirill.space.cell@tuta.io>
2026-04-18 18:12:41 +0500

    feat: init App

---
+++ store-app/src/main/java/com/example/store/StoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class StoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
+}
